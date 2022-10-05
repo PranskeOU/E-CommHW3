4f9663e0a9c9bdf9a636b9671f6682728285f6a8
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-10-05 11:45:04 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
+    <h5 style="text-align:center; margin:auto">I created this as a simulation of what a Big Event database might look like</h5>
 <table class="table table-striped">
   <thead>
     <tr>
