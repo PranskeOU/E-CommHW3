750fb3f3330da67bd02df933498666eaf9fc1bcd
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-10-05 14:01:53 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,11 +3,10 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Pranske Homework 3</title>
+    <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
-    <h5 style="text-align:center; margin:auto">I created this as a simulation of what a Big Event database might look like</h5>
 <table class="table table-striped">
   <thead>
     <tr>
@@ -29,7 +28,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT jobsite_id, jobsite_name from jobsite";
+$sql = "SELECT instructor_id, instructor_name from instructor";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
@@ -37,8 +36,8 @@ if ($result->num_rows > 0) {
   while($row = $result->fetch_assoc()) {
 ?>
   <tr>
-    <td><?=$row["jobsite_id"]?></td>
-    <td><?=$row["jobsite_name"]?></td>
+    <td><?=$row["instructor_id"]?></td>
+    <td><?=$row["instructor_name"]?></td>
   </tr>
 <?php
   }
@@ -49,6 +48,14 @@ $conn->close();
 ?>
   </tbody>
     </table>
+    <h1>Hello, world!</h1>
+    <form action="handlepost.php" method="get">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+    
+    <a class="btn btn-primary" href="page2.html" role="button">Page 2</a>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
