29ffe1c8dc00962f2ab27036287ad9d0743d4f04
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-10-10 14:06:27 -0500

    Update section-edit.php

--- section-edit.php
+++ section-edit.php
@@ -1,1 +1,45 @@
+<?php require_once("header.php"); ?>
+    <h1>Edit Section</h1>
+<?php
+$servername = "localhost:3306";
+$username = "pranskeo_homework3";
+$password = "iAYlcs$15a!4";
+$dbname = "pranskeo_homework3";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * from section where section_id=?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $_POST['id']);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
+<form method="post" action="section-edit-save.php">
+  <div class="mb-3">
+    <label for="instructorName" class="form-label">Name</label>
+    <input type="text" class="form-control" id="instructorName" aria-describedby="nameHelp" name="iName" value="<?=$row['instructor_name']?>">
+    <div id="nameHelp" class="form-text">Enter the instructor's name.</div>
+  </div>
+  <input type="hidden" name="iid" value="<?=$row['instructor_id']?>">
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
