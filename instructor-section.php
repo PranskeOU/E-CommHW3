805af2d2c950c7affa9e3721620dd667f4a2c641
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-10-10 12:52:33 -0500

    Update instructor-section.php

--- instructor-section.php
+++ instructor-section.php
@@ -1,12 +1,4 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Sections</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-  </head>
-  <body>
+<?php require_once("header.php"); ?>
     <h1>Sections</h1>
 <table class="table table-striped">
   <thead>
@@ -33,7 +25,7 @@ if ($conn->connect_error) {
 }
 $iid = $_GET['id'];
 //echo $iid;
-$sql = "select section_id, section_number, i.instructor_name, c.prefix, c.number from section s join instructor i on i.instructor_id = s.instructor_id join course c on c.course_id = s.course_id where i.instructor_id=?";
+$sql = "select section_id, i.instructor_name, c.prefix, c.number from section s join instructor i on i.instructor_id = s.instructor_id join course c on c.course_id = s.course_id where i.instructor_id=?";
 //echo $sql;
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("i", $iid);
@@ -48,7 +40,7 @@ if ($result->num_rows > 0) {
     <td><?=$row["section_id"]?></td>
     <td><?=$row["prefix"]?></td>
     <td><?=$row["number"]?></td>
-    <td><?=$row["section_number"]?></td>
+    <td>00<?=$row["section_id"]?></td>
     <td><?=$row["instructor_name"]?></td>
   </tr>
 <?php
