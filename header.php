5acbb1098a1721c3f14abf04274512f3ced7f912
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-10-10 13:47:26 -0500

    Update header.php

--- header.php
+++ header.php
@@ -15,6 +15,10 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="instructors.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Instructors
+          </a>
         <ul class="dropdown-menu">
           <li><a class="dropdown-item" href="instructors.php">All Instructors</a></li>
           <li><hr class="dropdown-divider"></li>
