286f77ea973442f9e7cd5d8f3558326c32cda472
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-10-25 16:42:02 -0500

    Update sections.php

--- sections.php
+++ sections.php
@@ -24,6 +24,33 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
+      
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  switch ($_POST['saveType']) {
+    case 'Add':
+      $sqlAdd = "insert into section (course_id, instructor_id) values (?,?)";
+      $stmtAdd = $conn->prepare($sqlAdd);
+      $stmtAdd->bind_param("ii", $_POST['scourse_id'], $_POST['sinstructor_id']);
+      $stmtAdd->execute();
+      echo '<div class="alert alert-success" role="alert">New section added.</div>';
+      break;
+    case 'Edit':
+      $sqlEdit = "update section set course_id=?, instructor_id=? where section_id=?";
+      $stmtEdit = $conn->prepare($sqlEdit);
+      $stmtEdit->bind_param("iii", $_POST['scourse_id'], $_POST['sinstructor_id'], $_POST['ssection_id']);
+      $stmtEdit->execute();
+      echo '<div class="alert alert-success" role="alert">Section edited.</div>';
+      break;
+    case 'Delete':
+      $sqlDelete = "delete from course where course_id=?";
+      $stmtDelete = $conn->prepare($sqlDelete);
+      $stmtDelete->bind_param("i", $_POST['ssection_id']);
+      $stmtDelete->execute();
+      echo '<div class="alert alert-success" role="alert">Course deleted.</div>';
+      break;
+  }
+}
+
 
 $sql = "select section_id, i.instructor_name, c.prefix, c.number from section s join instructor i on i.instructor_id = s.instructor_id join course c on c.course_id = s.course_id";
 $result = $conn->query($sql);
@@ -38,6 +65,18 @@ if ($result->num_rows > 0) {
     <td><?=$row["number"]?></td>
     <td>00<?=$row["section_id"]?></td>
     <td><?=$row["instructor_name"]?></td>
+   <td>
+      <button type="button" class="btn btn-light" data-bs-toggle="modal" data-bs-target="#editCourse">
+        Edit
+      </button>
+      </td>
+    <td>
+      <form class="btn btn-light" method="post" action="">
+         <input type="hidden" name="course_id" value="<?=$row["ssection_id"]?>" />
+         <input type="hidden" name="saveType" value="Delete">
+         <input type="submit" class="btn" onclick="return confirm('Are you sure?')" value="Delete">
+       </form>
+    </td>
   </tr>
 <?php
   }
@@ -48,6 +87,40 @@ $conn->close();
 ?>
   </tbody>
     </table>
+<div class="modal fade" id="editSection" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="editSectionLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="editSectionLabel">Edit Section</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <form method="post" action="">
+                <div class="mb-3">
+                  <label for="scourse_id" class="form-label">Instructor ID</label>
+                  <input type="text" class="form-control" id="scourse_id" aria-describedby="scourse_idHelp" name="scourse_id">
+                  <div id="scourse_idHelp" class="form-text">Enter the course ID</div>
+                </div>
+                <input type="hidden" name="saveType" value="Edit">
+              </form>
+            </div>
+            <div class="modal-body">
+              <form method="post" action="">
+                <div class="mb-3">
+                  <label for="sinstructor_id" class="form-label">Instructor ID</label>
+                  <input type="text" class="form-control" id="scourse_id" aria-describedby="sinstructor_idHelp" name="sinstructor_id">
+                  <div id="sinstructor_idHelp" class="form-text">Enter the instructor ID</div>
+                </div>
+                <input type="hidden" name="saveType" value="Edit">
+              </form>
+            </div>
+                <input type="hidden" name="saveType" value="Edit">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
