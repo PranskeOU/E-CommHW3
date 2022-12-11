7b317042d4416bb1882bf29a9ab601080247aa22
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-12-11 17:25:41 -0600

    ;ilhrfgd

--- courses.php
+++ courses.php
@@ -84,8 +84,9 @@ if ($result->num_rows > 0) {
                 <input type="text" class="form-control" id="editCourse<?=$row["course_id"]?>Number" aria-describedby="editCourse<?=$row["course_id"]?>Help" name="Number" value="<?=$row["number"]?>">
                 <label for="editCourse<?=$row["course_id"]?>Description" class="form-label">Course Description</label>
                 <input type="text" class="form-control" id="editCourse<?=$row["course_id"]?>Description" aria-describedby="editCourse<?=$row["course_id"]?>Help" name="Description" value="<?=$row["description"]?>">
-                <input type="hidden" name="course_id" value="<?=$row["course_id"]?>"
+                <input type="hidden" name="course_id" value="<?=$row["course_id"]?>">
                 <input type="hidden" name="saveType" value="Edit">
+                <div></div>
                 <button type="submit" class="btn btn-primary" value="Submit">Submit</button>
               </form>
             </div>
