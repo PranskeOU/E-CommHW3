38c4234556568c7faaa77e786b743ee87d5ed2a6
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-12-12 13:53:37 -0600

    dfgsd

--- sections.php
+++ sections.php
@@ -28,25 +28,25 @@ if ($conn->connect_error) {
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   switch ($_POST['saveType']) {
     case 'Add':
-      $sqlAdd = "insert into section (course_id, instructor_id) values (?,?)";
+      $sqlAdd = "insert into section (section_id, instructor_id) values (?,?)";
       $stmtAdd = $conn->prepare($sqlAdd);
-      $stmtAdd->bind_param("ii", $_POST['course_id'], $_POST['instructor_id']);
+      $stmtAdd->bind_param("ii", $_POST['section_id'], $_POST['instructor_id']);
       $stmtAdd->execute();
       echo '<div class="alert alert-success" role="alert">New section added.</div>';
       break;
     case 'Edit':
-      $sqlEdit = "update section set course_id=?, instructor_id=? where section_id=?";
+      $sqlEdit = "update section set section_id=?, instructor_id=? where section_id=?";
       $stmtEdit = $conn->prepare($sqlEdit);
-      $stmtEdit->bind_param("iii", $_POST['course_id'], $_POST['instructor_id'], $_POST['section_id']);
+      $stmtEdit->bind_param("iii", $_POST['section_id'], $_POST['instructor_id'], $_POST['section_id']);
       $stmtEdit->execute();
       echo '<div class="alert alert-success" role="alert">Section edited.</div>';
       break;
     case 'Delete':
-      $sqlDelete = "delete from course where course_id=?";
+      $sqlDelete = "delete from section where section_id=?";
       $stmtDelete = $conn->prepare($sqlDelete);
       $stmtDelete->bind_param("i", $_POST['section_id']);
       $stmtDelete->execute();
-      echo '<div class="alert alert-success" role="alert">Course deleted.</div>';
+      echo '<div class="alert alert-success" role="alert">Section deleted.</div>';
       break;
   }
 }
@@ -69,10 +69,34 @@ if ($result->num_rows > 0) {
       <button type="button" class="btn btn-light" data-bs-toggle="modal" data-bs-target="#editSection">
         Edit
       </button>
+      <div class="modal fade" id="editSection<?=$row["section_id"]?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="editSection<?=$row["section_id"]?>Label">" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="editSection<?=$row["section_id"]?>Label">Edit Section</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <form method="post" action="">
+                <div class="mb-3">
+                <label for="editSection<?=$row["section_id"]?>course_id" class="form-label">New Course ID</label>
+                <input type="text" class="form-control" id="editSection<?=$row["section_id"]?>course_id" aria-describedby="editSection<?=$row["section_id"]?>Help" name="Prefix" value="<?=$row["prefix"]?>">
+                <label for="editSection<?=$row["section_id"]?>instructor_id" class="form-label">New Instructor ID</label>
+                <input type="text" class="form-control" id="editSection<?=$row["section_id"]?>instructor_id" aria-describedby="editSection<?=$row["section_id"]?>Help" name="Number" value="<?=$row["number"]?>">
+                <input type="hidden" name="section_id" value="<?=$row["section_id"]?>">
+                <input type="hidden" name="saveType" value="Edit">
+                <br></br>
+                <button type="submit" class="btn btn-primary" value="Submit">Submit</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      </td>
       </td>
     <td>
       <form class="btn btn-light" method="post" action="">
-         <input type="hidden" name="course_id" value="<?=$row["section_id"]?>" />
+         <input type="hidden" name="section_id" value="<?=$row["section_id"]?>" />
          <input type="hidden" name="saveType" value="Delete">
          <input type="submit" class="btn" onclick="return confirm('Are you sure?')" value="Delete">
        </form>
@@ -87,40 +111,7 @@ $conn->close();
 ?>
   </tbody>
     </table>
-<div class="modal fade" id="editSection" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="editSectionLabel" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h1 class="modal-title fs-5" id="editSectionLabel">Edit Section</h1>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-              <form method="post" action="">
-                <div class="mb-3">
-                  <label for="scourse_id" class="form-label">New Course ID</label>
-                  <input type="text" class="form-control" id="scourse_id" aria-describedby="scourse_idHelp" name="scourse_id">
-                  <div id="scourse_idHelp" class="form-text">Enter the course ID</div>
-                </div>
-                <input type="hidden" name="saveType" value="Edit">
-              </form>
-            </div>
-            <div class="modal-body">
-              <form method="post" action="">
-                <div class="mb-3">
-                  <label for="sinstructor_id" class="form-label">New Instructor ID</label>
-                  <input type="text" class="form-control" id="scourse_id" aria-describedby="sinstructor_idHelp" name="sinstructor_id">
-                  <div id="sinstructor_idHelp" class="form-text">Enter the instructor ID</div>
-                </div>
-                <input type="hidden" name="saveType" value="Edit">
-              </form>
-            </div>
-                <input type="hidden" name="saveType" value="Edit">
-                <button type="submit" class="btn btn-primary">Submit</button>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
